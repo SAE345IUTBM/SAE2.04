1e1206afac1821dbc735e52fda70b3eb1dda9280
SAE345IUTBM <98029363+SAE345IUTBM@users.noreply.github.com>
2022-02-04 08:55:06 +0100

    Update bdd_sql_v1.sql

--- bdd_sql_v1.sql
+++ bdd_sql_v1.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS Depose, commande, etat, panier, ligne_commande, user, Velo, Fournisseur, Type_velo, Couleur, Marque;
+
 CREATE TABLE Marque(
    id_marque INT AUTO_INCREMENT,
    libelle_marque VARCHAR(255),
@@ -102,7 +104,7 @@ CREATE TABLE Ligne_commande(
    FOREIGN KEY(id_commande) REFERENCES Commande(id_commande)
 );
 
-CREATE TABLE Dépose(
+CREATE TABLE Depose(
    id_velo INT,
    id INT,
    id_avis INT,
@@ -111,3 +113,63 @@ CREATE TABLE Dépose(
    FOREIGN KEY(id) REFERENCES utilisateur(id),
    FOREIGN KEY(id_avis) REFERENCES Avis(id_avis)
 );
+
+INSERT INTO Marque VALUES
+(NULL, 'Cannondale'),
+(NULL, 'BMC'),
+(NULL, 'Lapierre'),
+(NULL, 'Btwin'),
+(NULL, 'Pinarello'),
+(NULL, 'Nakamura'),
+(NULL, 'Rockrider'),
+(NULL, 'Trek');
+
+INSERT INTO Couleur VALUES
+(NULL, 'Noir'),
+(NULL, 'Blanc'),
+(NULL, 'Gris'),
+(NULL, 'Jaune'),
+(NULL, 'Vert'),
+(NULL, 'Bleu'),
+(NULL, 'Rouge'),
+(NULL, 'Sable');
+
+INSERT INTO Type_velo VALUES
+(NULL, 'VTT'),
+(NULL, 'VTC'),
+(NULL, 'Route');
+
+INSERT INTO Fournisseur VALUES
+(NULL, 'Decathlon'),
+(NULL, 'Intersport'),
+(NULL, 'Alltricks'),
+(NULL, 'Wareega'),
+(NULL, 'Probikeshop'),
+(NULL, 'Bikester');
+
+INSERT INTO Materiaux VALUES
+(NULL, 'Aluminium'),
+(NULL, 'Acier'),
+(NULL, 'Carbone'),
+(NULL, 'Titane');
+
+INSERT INTO Velo VALUES
+(NULL, 'Dirt Jump Dave', 31, 8, 1100, 'Cannondale_1.png', 4219, 1, 3, 1, 2, 4),
+(NULL, 'F-Si Carbon 5', 31, 8, 1800, 'Cannondale_2.png', 3830, 1, 4, 1, 3, 4),
+(NULL, 'Team Machine SLR', 29, 7.1, 8999, 'BMC_1.png', 737, 2, 3, 3, 5, 3),
+(NULL, 'Road Machine', 29, 7.1, 4899, 'BMC_2.png', 1152, 2, 8, 3, 6, 3),
+(NULL, 'Edge 3.9', 29, 11, 599, 'Lapierre_1.jpg', 11072,  3, 3, 1, 3, 1),
+(NULL, 'Xelius 8.0', 31, 9, 5399, 'Lapierre_2.png', 2384, 3, 7, 3, 5, 3),
+(NULL, 'Riverside 500', 26, 15, 299, 'Btwin_1.jpg', 18411, 4, 3, 2, 1, 2),
+(NULL, 'GAN Dura-Ace', 29, 10, 4999, 'Pinarello_1.jpg', 2139, 5, 1, 3, 3, 3),
+(NULL, 'X2', 28, 11, 399, 'Nakamura_1.png', 16486, 6, 1, 2, 2, 1),
+(NULL, 'E-ST 100', 27.5, 22.5, 999, 'Rockrider_1.jpg', 6423, 7, 6, 1, 1, 1),
+(NULL, 'E-ST 520', 27.5, 22.7, 1799, 'Rockrider_2.jpg', 3159, 7, 2, 1, 1, 1),
+(NULL, 'E-ST 500', 27.5, 22.2, 1299, 'Rockrider_3.jpg', 3840, 7, 6, 1, 1, 1),
+(NULL, 'MARLIN 8 SRAM SX', 27.5, 13.2, 1024, 'Trek_1.jpg', 7108, 8, 7, 1, 6, 1),
+(NULL, 'Verve+ 3', 29, 24, 3000, 'Trek_2.png', 1982, 7, 3, 2, 4, 1);
+
+INSERT INTO user (id, email, username, password, role,  est_actif) VALUES
+(NULL, 'admin@admin.fr', 'admin', 'sha256$pBGlZy6UukyHBFDH$2f089c1d26f2741b68c9218a68bfe2e25dbb069c27868a027dad03bcb3d7f69a', 'ROLE_admin', 1),
+(NULL, 'client@client.fr', 'client', 'sha256$Q1HFT4TKRqnMhlTj$cf3c84ea646430c98d4877769c7c5d2cce1edd10c7eccd2c1f9d6114b74b81c4', 'ROLE_client', 1),
+(NULL, 'client2@client2.fr', 'client2', 'sha256$ayiON3nJITfetaS8$0e039802d6fac2222e264f5a1e2b94b347501d040d71cfa4264cad6067cf5cf3', 'ROLE_client',1);
