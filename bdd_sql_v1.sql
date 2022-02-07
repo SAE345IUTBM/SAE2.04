f976099fc8bdf9163891fe514adb08417b93cf07
SAE345IUTBM <98029363+SAE345IUTBM@users.noreply.github.com>
2022-02-07 10:24:19 +0100

    Update bdd_sql_v1.sql

--- bdd_sql_v1.sql
+++ bdd_sql_v1.sql
@@ -188,3 +188,9 @@ INSERT INTO user (id, email, username, password, role,  est_actif) VALUES
 (NULL, 'admin@admin.fr', 'admin', 'sha256$pBGlZy6UukyHBFDH$2f089c1d26f2741b68c9218a68bfe2e25dbb069c27868a027dad03bcb3d7f69a', 'ROLE_admin', 1),
 (NULL, 'client@client.fr', 'client', 'sha256$Q1HFT4TKRqnMhlTj$cf3c84ea646430c98d4877769c7c5d2cce1edd10c7eccd2c1f9d6114b74b81c4', 'ROLE_client', 1),
 (NULL, 'client2@client2.fr', 'client2', 'sha256$ayiON3nJITfetaS8$0e039802d6fac2222e264f5a1e2b94b347501d040d71cfa4264cad6067cf5cf3', 'ROLE_client',1);
+
+INSERT INTO etat (libelle_etat) VALUES
+('En attente'),
+('Expédié')
+('Validé'),
+('Confirmé');
